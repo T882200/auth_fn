de0fad6b2f6c0de1c8780c01e614d06920f5e4b9
t8822_000 <t882200@gmail.com>
2019-01-25 06:36:49 +0200

    first commit - reuse of several components, installing firebase package, setup connection between firebase and my app

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import firebase from 'firebase';
+import { View } from 'react-native';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+// firebase setup
+
+/* <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCwuUAlNk4GZRmyVWemxm1C0CvSbXRDUFc",
+    authDomain: "loginapp-4c855.firebaseapp.com",
+    databaseURL: "https://loginapp-4c855.firebaseio.com",
+    projectId: "loginapp-4c855",
+    storageBucket: "loginapp-4c855.appspot.com",
+    messagingSenderId: "973637920890"
+  };
+  firebase.initializeApp(config);
+</script> */
+
+
+class App extends Component {
+  
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCwuUAlNk4GZRmyVWemxm1C0CvSbXRDUFc",
+      authDomain: "loginapp-4c855.firebaseapp.com",
+      databaseURL: "https://loginapp-4c855.firebaseio.com",
+      projectId: "loginapp-4c855",
+      storageBucket: "loginapp-4c855.appspot.com",
+      messagingSenderId: "973637920890"
+    });
+  }
+  
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <LoginForm />
+      </View>
+    )
+  }
+}
+
+export default App;
